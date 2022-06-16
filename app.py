63a10120b2bfadc3e6ec46dde2c7c7cc4234e98a
ajm1102 <ajm1102.moore@gmail.com>
2022-06-16 22:50:31 +0100

    'Redirects'

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
+
+@app.route('/', methods=['POST', 'GET'])
+def Simulation():
+    if request.method == "POST":
+        user = request.form['nm']
+        session["user"] = user
+        return redirect(url_for("results"))
+    else:
+        return render_template("index.html")
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+@app.route('/results')
+def results():
+    if "user" in session:
+        user = session["user"]
+        user = user + 'XYZ'
+        return render_template('display.html', content=user)
+    else:
+        return redirect(url_for('Simulation'))
 
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    return render_template('')
-
-
-@app.route('/user')
-def user(usr):
-    return
 
 
 if __name__ == '__main__':
