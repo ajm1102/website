178eb1670aadd6679167fe048b16f27677e3b603
ajm1102 <ajm1102.moore@gmail.com>
2022-06-16 17:26:37 +0100

    icon

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('')
+
+
+@app.route('/user')
+def user(usr):
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
